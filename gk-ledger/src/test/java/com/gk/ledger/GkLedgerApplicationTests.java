9589a835d912dd4b86b87196622fc78d8f81e241
gyh7790 <gyh7790@gmail.com>
2026-06-06 22:08:24 +0800

    build: wire ledger module into reactor

---
+++ gk-ledger/src/test/java/com/gk/ledger/GkLedgerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gk.ledger;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class GkLedgerApplicationTests {
+
+    @Test
+    void moduleNameIsStable() {
+        assertEquals("gk-ledger", "gk-ledger");
+    }
+}

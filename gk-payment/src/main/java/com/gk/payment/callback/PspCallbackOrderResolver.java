ee22a4bac6ffffc234efc0916efcb0af52f11c8c
gyh7790 <gyh7790@gmail.com>
2026-07-03 01:14:05 +0800

    调整 openapi 响应 + 1

--- gk-payment/src/main/java/com/gk/payment/callback/PspCallbackOrderResolver.java
+++ gk-payment/src/main/java/com/gk/payment/callback/PspCallbackOrderResolver.java
@@ -58,7 +58,8 @@ public class PspCallbackOrderResolver {
                 order.getId(), order.getTenantId(), order.getMerchantId(), order.getMerchantNo(),
                 order.getMerchantAppId(), order.getAppId(), order.getPspId(), order.getPspCode(),
                 order.getPspAccountId(), apiSecret, order.getPayinOrderNo(), order.getMerchantOrderNo(),
-                order.getPspOrderNo(), order.getStatus(), order.getStatusReason(), order.getCountryCode(),
+                order.getPspOrderNo(), order.getStatus(), order.getStatusReason(),
+                order.getMerchantStatusCode(), order.getMerchantStatusReason(), order.getCountryCode(),
                 order.getMethodCode(), order.getAmount(), order.getMerchantFeeAmount(),
                 order.getSettleAmount(), null, order.getCurrency(), order.getNotifyUrl()
         );
@@ -72,7 +73,8 @@ public class PspCallbackOrderResolver {
                 order.getId(), order.getTenantId(), order.getMerchantId(), order.getMerchantNo(),
                 order.getMerchantAppId(), order.getAppId(), order.getPspId(), order.getPspCode(),
                 order.getPspAccountId(), apiSecret, order.getPayoutOrderNo(), order.getMerchantOrderNo(),
-                order.getPspOrderNo(), order.getStatus(), order.getStatusReason(), order.getCountryCode(),
+                order.getPspOrderNo(), order.getStatus(), order.getStatusReason(),
+                order.getMerchantStatusCode(), order.getMerchantStatusReason(), order.getCountryCode(),
                 order.getMethodCode(), order.getAmount(), order.getMerchantFeeAmount(),
                 null, order.getTotalDebitAmount(), order.getCurrency(), order.getNotifyUrl()
         );

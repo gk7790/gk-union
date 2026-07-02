60cb87689df4289d69095e723fdd804215e84a89
gyh7790 <gyh7790@gmail.com>
2026-07-02 13:47:56 +0800

    优化 PayoutSubmitConfig 的参数

--- gk-infra/src/main/java/com/gk/infra/config/model/PayoutSubmitConfig.java
+++ gk-infra/src/main/java/com/gk/infra/config/model/PayoutSubmitConfig.java
@@ -7,4 +7,5 @@ public class PayoutSubmitConfig {
     private boolean asyncSubmit = true;
     private int defaultBatchSize = 20;
     private long firstQueryDelaySeconds = 60;
+    private int maxRouteAttempts = 3;
 }
